9502f28ad1b4dbbf72f705531c4c55fc2a70c8b8
Bashir535 <168224922+Bashir535@users.noreply.github.com>
2025-12-03 10:27:39 -0800

    Update ProtectedRoute.jsx

--- frontend/src/shared/ProtectedRoute.jsx
+++ frontend/src/shared/ProtectedRoute.jsx
@@ -1,7 +1,7 @@
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
 
-// This is to protect admin pages
+
 export default function ProtectedRoute({ children }) {
     const { user } = useSelector((state) => state.auth);
     if (!user.admin) {
